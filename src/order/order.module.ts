f5732435820f1ebd57b5ce68799cddb4559f7483
bouzidi48 <mohemmedbouzidi16@gmail.com>
2024-07-03 10:08:32 +0200

    jsd

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { UserModule } from 'src/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { OrderRepository } from './order.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Order,OrderRepository]),UserModule],
   controllers: [OrderController],
   providers: [OrderService],
 })
